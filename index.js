9793f82af6b4cad505f2a0d4116c98b4fd598d12
sunnisacoder <funsunny1107@gmail.com>
2024-09-04 14:22:14 +0800

    main function done

--- index.js
+++ index.js
@@ -1,9 +1,52 @@
+let listState = [];
+
+const STATE_KEY = "todo-list";
+
+function loadState() {
+    const listState = localStorage.getItem(STATE_KEY);
+    if (listState !== null) {
+        return JSON.parse(listState);
+    }
+    return [];
+}
+
+function saveState(list) {
+    localStorage.setItem(STATE_KEY, JSON.stringify(list));
+}
+
+function initList() {
+    //load State
+    listState = loadState();
+    // render list
+    const ul = document.getElementById("listItem");
+    for (const item of listState) {
+        const li = document.createElement("li");
+        li.innerText = item.text;
+
+        const deleteBtn = document.createElement("span");
+        deleteBtn.classList.add("delete")
+        deleteBtn.onclick = deleteItem;
+        li.appendChild(deleteBtn);
+
+        li.classList.add("item");
+        if (item.checked) {
+            li.classList.add("checked");
+        }
+
+        console.log("initList called");
+
+        li.onclick = checkItem;
+
+        ul.appendChild(li);
+    }
+}
+
 //測試用
 // console.log("cat");
 
 //防止刷新，防止form預設點擊送出的功能
 const form = document.getElementById("formWrap");
-form.addEventListener("submit",(e)=>{
+form.addEventListener("submit", (e) => {
     e.preventDefault();
 })
 
@@ -15,7 +58,7 @@ function addItem() {
     const ul = document.getElementById("listItem");
     const input = document.getElementById("formInput");
     const text = input.value;
-    if (text === ""){
+    if (text === "") {
         alert("輸入東西拉bro");
         return;
     }
@@ -24,31 +67,51 @@ function addItem() {
     newItem.classList.add("item");
     newItem.innerText = text;
 
-    newItem.onclick = checkItem; 
+    newItem.onclick = checkItem;
 
     const deleteBtn = document.createElement("span");
     deleteBtn.classList.add("delete");
-    deleteBtn.onclick= deleteItem;
+    deleteBtn.onclick = deleteItem;
 
     newItem.appendChild(deleteBtn);
 
-    input.value="";
+    listState.push({
+        text,
+        checked: false
+    });
+    saveState(listState);
+
+    input.value = "";
     ul.appendChild(newItem);
 }
 
-function checkItem(){
+function checkItem() {
     // console.log("already checked");
+    // console.log("Item checked:", this.innerText);
     const item = this;
+    const parent = item.parentNode;
+    const idx = Array.from(parent.children).indexOf(item);
+
+    listState[idx].checked = !listState[idx].checked;
     item.classList.toggle("checked");
+    saveState(listState);
 }
 
-function deleteItem(){
+function deleteItem() {
     // console.log("3 la");
+    event.stopPropagation();
     const item = this.parentNode;
     const parent = item.parentNode;
+    const idx = Array.from(parent.childNodes).indexOf(item);
+    listState = listState.filter((_, i) => i !== idx);
+    saveState(listState);
+
     parent.removeChild(item);
 }
 
+initList();
 // 連結檔案，log看看
 // 防止瀏覽器刷新
 // 開始建立第一個function Name(){要做的事情} -> 先log看看
+
+
